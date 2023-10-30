458ae3326a90ec089856cdb4bae5f9ca13e2f3dc
Himanshu Singh <himanshu.developer01@gmail.com>
2023-10-30 11:18:56 +0530

    added canSubmit and validateFld method

--- src/js/index.js
+++ src/js/index.js
@@ -5,3 +5,20 @@ const repasswordFld = document.querySelector("input[name='repassword']");
 const fullnameFld = document.querySelector("input[name='fullname']");
 const merchantFld = document.querySelector("input[name='merchant']");
 const passwordRegEx = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm;
+
+let canSubmit = true;
+
+const submitForm = (data) => {
+  console.log(data);
+  //formEl.submit();
+};
+
+const validateFld = (el, condition) => {
+  if (condition) {
+    canSubmit = true;
+    el.parentElement.classList.remove('form-field-error');
+  } else {
+    canSubmit = false;
+    el.parentElement.classList.add('form-field-error');
+  }
+};
